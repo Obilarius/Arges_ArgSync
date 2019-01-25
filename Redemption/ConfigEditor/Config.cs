418f3c80ec154c0df5dad375c808778949a365fb
Obilarius <git@walzenbach.com>
2019-01-25 11:06:56 +0100

    new ConfigEditor

--- Redemption/ConfigEditor/Config.cs
+++ Redemption/ConfigEditor/Config.cs
@@ -11,25 +11,24 @@ namespace ConfigEditor
         public string password, username, domain, exUri;
         public List<configMailboxes> mailboxes = new List<configMailboxes>();
 
-        public void AddMailbox(string v1, string v2)
+        public void AddMailbox(string smtpAdresse, bool birthday, bool anniversary, List<string> folder, bool disable = false)
         {
-            mailboxes.Add(new configMailboxes(v1, v2));
-        }
-
-        public void AddMailbox(string v1, string v2, bool disable)
-        {
-            mailboxes.Add(new configMailboxes(v1, v2, disable));
+            mailboxes.Add(new configMailboxes(smtpAdresse, birthday, anniversary, folder, disable));
         }
     }
 
     public class configMailboxes
     {
-        public string smtpAdresse, folder;
+        public string smtpAdresse;
+        public bool birthday, anniversary;
+        public List<string> folder = new List<string>();
         public bool disable;
 
-        public configMailboxes(string smtpAdresse, string folder, bool disable = false)
+        public configMailboxes(string smtpAdresse, bool birthday, bool anniversary, List<string> folder, bool disable)
         {
             this.smtpAdresse = smtpAdresse;
+            this.birthday = birthday;
+            this.anniversary = anniversary;
             this.folder = folder;
             this.disable = disable;
         }
