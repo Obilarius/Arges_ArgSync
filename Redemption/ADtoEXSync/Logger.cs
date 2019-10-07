25bce829902cabd52b3b2088da839d0e57121d7a
Obilarius <git@walzenbach.com>
2019-10-07 14:17:50 +0200

    Logs werden erstellt

---
+++ Redemption/ADtoEXSync/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADtoEXSync
+{
+    static class Logger
+    {
+        public static void writeLog(string logText)
+        {
+            string binaryPath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            var path = binaryPath + @"\log";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            var log = DateTime.Now.ToShortDateString() + "-" + DateTime.Now.ToLongTimeString() + " - " + logText;
+            var filename = path + @"\" + DateTime.Now.ToString("yyyy-MM") + "_Log.txt";
+            StreamWriter SyncStateWriter = new StreamWriter(filename, true);
+            SyncStateWriter.WriteLine(log);
+            SyncStateWriter.Close();
+        }
+    }
+}
